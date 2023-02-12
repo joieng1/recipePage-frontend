49635aea3328f66f9785f893cda163196e605f50
joieng1 <johncieng1@gmail.com>
2023-02-12 15:00:29 -0800

    feat: implement loading during api call

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,7 +13,10 @@ interface Recipe {
 function Home() {
     const [externalRecipes, setExternalRecipes] = useState<Recipe[]>([]);
     const [myRecipes, setMyRecipes] = useState<Recipe[]>([]);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
+        setLoading(true);
         fetch("https://bootcamp-milestone-4.onrender.com/recipe")
             .then((res) => res.json())
             .then((data) => {
@@ -23,11 +26,13 @@ function Home() {
             .then((res) => res.json())
             .then((data) => {
                 setMyRecipes(data);
+                setLoading(false);
             });
     }, []);
     return (
         <main>
             <h1 className="title">Check out some of our favorites!</h1>
+            <h1 className="loading">{loading ? <>Loading...</> : <></>}</h1>
             {/* <!-- list of recipes --> */}
             <div className="container-recipes">
                 {myRecipes.map((recipe) => (
